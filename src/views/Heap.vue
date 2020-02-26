1bf8b3da06de9c2bc3112eaa63e5a21701ef1ce0
Joshua Coda <joshua.coda@gmail.com>
2020-02-26 07:30:04 -0800

    added

--- src/views/Heap.vue
+++ src/views/Heap.vue
@@ -36,37 +36,46 @@
           </thead>
           <tbody>
             <tr v-for="item in heap" :key="item.name">
-              <td>{{ item.name }}</td>
-              <td>{{ item.calories }}</td>
+              <td>{{ item.cocNumber }}</td>
+              <td>{{ item.jurisdiction }}</td>
+              <td>{{ item.totalHomeless }}</td>
+              <td>{{ item.percentHomeless }}</td>
+              <td>{{ item.perJurisdiction }}</td>
+              <td>{{ item.distribution }}</td>
+              <td>{{ item.total }}</td>
+              <td>{{ item.youthTotal }}</td>
             </tr>
           </tbody>
         </template>
       </v-simple-table>
       <div>
-        <div class="caption">
+        <div class="caption my-5">
           Source: https://www.bcsh.ca.gov/hcfc/documents/heap_overview.pdf
         </div>
 
-        <div class="body-1">
-          About the Homeless Emergency Aid Program The Homeless Emergency Aid
-          Program was established by statute to provide localities with flexible
-          block grant funds to address their immediate homelessness challenges.
-          The Homeless Emergency Aid Block Grants will provide $500 million in
-          one-time funding to enable local governments to respond to
-          homelessness. Allocations are as follows: $250 million to Continuums
-          of Care based on 2017 homeless point in time count; $150 million
-          direct allocation to a city or city that is also a county with a
-          population of 330,000 or more as of January 1, 2018; and $100 million
-          to Continuums of Care based on their percentage of the statewide 2017
-          homeless population. Requirements: City, county, or joint power must
-          declared an emergency shelter crisis (waiver process for smaller
-          cities and counties that do not declare a shelter emergency).
-          Continuum of Care must demonstrate collaboration with other city,
-          county, or nonprofit partners. Eligible Activities: Emergency housing
-          vouchers Rapid rehousing Emergency shelter construction, and Use of
-          armories to provide temporary shelters, among other activities For
-          more information, contact: hcfc@bcsh.ca.gov.
-          <div class="caption">
+        <div class="body-1 my-5">
+          Roune 1 Standard Distribution of Funds Began Apr 2019. Round 2
+          Standard Distribution of Funds Began July 2019. 100 percent of the
+          funds must be expended by June 30, 2021. About the Homeless Emergency
+          Aid Program The Homeless Emergency Aid Program was established by
+          statute to provide localities with flexible block grant funds to
+          address their immediate homelessness challenges. The Homeless
+          Emergency Aid Block Grants will provide $500 million in one-time
+          funding to enable local governments to respond to homelessness.
+          Allocations are as follows: $250 million to Continuums of Care based
+          on 2017 homeless point in time count; $150 million direct allocation
+          to a city or city that is also a county with a population of 330,000
+          or more as of January 1, 2018; and $100 million to Continuums of Care
+          based on their percentage of the statewide 2017 homeless population.
+          Requirements: City, county, or joint power must declared an emergency
+          shelter crisis (waiver process for smaller cities and counties that do
+          not declare a shelter emergency). Continuum of Care must demonstrate
+          collaboration with other city, county, or nonprofit partners. Eligible
+          Activities: Emergency housing vouchers Rapid rehousing Emergency
+          shelter construction, and Use of armories to provide temporary
+          shelters, among other activities For more information, contact:
+          hcfc@bcsh.ca.gov.
+          <div class="caption my-5">
             Source: https://www.bcsh.ca.gov/hcfc/aid_program.html
           </div>
         </div>
@@ -87,15 +96,31 @@ export default Vue.extend({
         href: "/"
       },
       {
-        text: "About",
+        text: "Funding",
+        disabled: false,
+        href: "/"
+      },
+      {
+        text: "State",
+        disabled: false,
+        href: "/"
+      },
+      {
+        text: "HEAP",
         disabled: true,
         href: "/"
       }
     ],
     heap: [
       {
-        name: "Frozen Yogurt",
-        calories: 159
+        cocNumber: "CA-608",
+        jurisdiction: "Riverside City & County",
+        totalHomeless: "2,406",
+        percentHomeless: "1.792%",
+        perJurisdiction: "$8,000,000.00",
+        distribution: "$1,791,805.06",
+        total: "$9,791,805.06",
+        youthTotal: "$489,590.25"
       }
     ]
   })
