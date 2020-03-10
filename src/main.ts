ba852ec4ee42f398694f561eb7821aea8d86fc2f
Joshua Coda <joshua.coda@gmail.com>
2020-03-09 19:31:51 -0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -20,14 +20,12 @@ Vue.filter("currency", function(value: number) {
 });
 
 (async () => {
-  let app;
-  await UserStore.init().then(() => {
-    if (!app) {
-      app = new Vue({
-        vuetify,
-        router,
-        render: h => h(App)
-      }).$mount("#app");
-    }
-  });
+  await UserStore.init();
+
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+  }).$mount("#app");
 })();
