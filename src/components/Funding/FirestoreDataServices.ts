85554e2c60f12a281078361ab66efe7b31a426bb
Joshua Coda <joshua.coda@gmail.com>
2020-03-17 17:33:02 -0700

    fix

---
+++ src/components/Funding/FirestoreDataServices.ts
@@ -0,0 +1,87 @@
+import { db } from "@/firebase";
+
+export class FirestoreDataServices {
+  getRecord(tableName: string, recordID: string, docToRecordMap: any) {
+    return new Promise(function(resolve, reject) {
+      const records: Array<object> = [];
+      db.collection(tableName)
+        .doc(recordID)
+        .get()
+        .then(function(doc: any) {
+          if (doc.exists) {
+            resolve(docToRecordMap(doc));
+          } else {
+            reject("Record not found");
+          }
+        })
+        .catch(function(error) {
+          reject(error);
+        });
+    });
+  }
+  getAll(tableName: string, recordID: string, docToSubDataMap: any) {
+    return new Promise(function(resolve, reject) {
+      const records: Array<object> = [];
+      db.collection(tableName)
+        .doc(recordID)
+        .collection("subData")
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            records.push(docToSubDataMap(doc));
+          });
+
+          resolve(records);
+        });
+    });
+  }
+}
+
+export class StatsRecord {
+  id = "";
+  amountTotal = "";
+  federalTotal = "";
+  stateTotal = "";
+  countyTotal = "";
+  localTotal = "";
+}
+export class SubDataRecord {
+  id = "";
+  source = "";
+  total = "";
+}
+
+export function DocToFundRecordMap(doc: any): StatsRecord {
+  const rowData = doc.data();
+  const record = {
+    id: doc.id,
+    amountTotal: rowData.amountTotal,
+    federalTotal: rowData.federalTotal,
+    stateTotal: rowData.stateTotal,
+    countyTotal: rowData.countyTotal,
+    localTotal: rowData.localTotal
+  };
+  return record;
+}
+export function DocToSubDataRecordMap(doc: any): SubDataRecord {
+  const rowData = doc.data();
+  const record = {
+    id: doc.id,
+    source: rowData.source,
+    total: rowData.total
+  };
+  return record;
+}
+
+export class StatsDataServices {
+  dataServices: FirestoreDataServices;
+  constructor() {
+    this.dataServices = new FirestoreDataServices();
+  }
+  GetStats(recordID: string) {
+    return this.dataServices.getRecord("funds", recordID, DocToFundRecordMap);
+  }
+  GetAll(recordID: string) {
+    return this.dataServices.getAll("funds", recordID, DocToSubDataRecordMap);
+  }
+}
--- src/components/Funding/FundingDashboardPie.vue
+++ src/components/Funding/FundingDashboardPie.vue
@@ -7,6 +7,7 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import { StatsDataServices } from "./FirestoreDataServices";
 
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
@@ -21,53 +22,23 @@ export default class FundingDashboardPie extends Vue {
   };
   private container: any;
 
-  private chartData: Array<object> = [
-    {
-      source: "Federal",
-      amount: 10000000.0,
-      subData: [
-        {
-          name: "CoC",
-          value: 9791805.06
-        }
-      ]
-    },
-    {
-      source: "State",
-      amount: 100000000.0,
-      subData: [
-        {
-          name: "CESH",
-          value: 9791805.06
-        },
-        {
-          name: "HEAP",
-          value: 9791805.06
-        }
-      ]
-    },
-    {
-      source: "County",
-      amount: 500000.0,
-      subData: [
-        {
-          name: "General",
-          value: 500000.0
-        }
-      ]
-    },
-    {
-      source: "Local",
-      amount: 100000.0,
-      subData: [
-        {
-          name: "City of Riverside",
-          value: 100000.0
-        }
-      ]
-    }
-  ];
-  public init(): void {
+  private e1 = "2019";
+  private chartData: Array<object> = [];
+
+  created() {
+    this.loadStats();
+  }
+  private loadStats() {
+    const statsDataService = new StatsDataServices();
+    const year = this.e1;
+    statsDataService.GetAll(year).then(data => {
+      this.chartData = data;
+      console.log(this.chartData);
+    });
+  }
+
+  public async init() {
+    console.log("start");
     const container = am4core.create(this.$refs.chartdiv, am4core.Container);
     container.width = am4core.percent(100);
     container.height = am4core.percent(100);
@@ -78,137 +49,20 @@ export default class FundingDashboardPie extends Vue {
     // Add data
 
     chart.data = this.chartData;
+    console.log(this.chartData);
 
     // Add and configure Series
-    const pieSeries: any = chart.series.push(new am4charts.PieSeries());
-    pieSeries.dataFields.value = "amount";
+    const pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = "total";
     pieSeries.dataFields.category = "source";
+    pieSeries.slices.template.stroke = am4core.color("#fff");
+    pieSeries.slices.template.strokeWidth = 2;
+    pieSeries.slices.template.strokeOpacity = 1;
 
-    pieSeries.slices.template.states.getKey(
-      "active"
-    ).properties.shiftRadius = 0;
-    pieSeries.labels.template.text =
-      "{category}\n{value.percent.formatNumber('#.#')}%";
-
-    pieSeries.slices.template.events.on("hit", function(event: any) {
-      // eslint-disable-next-line
-      selectSlice(event.target.dataItem);
-    });
-
-    const chart2 = container.createChild(am4charts.PieChart);
-    chart2.width = am4core.percent(30);
-    chart2.radius = am4core.percent(80);
-
-    // Add and configure Series
-    const pieSeries2: any = chart2.series.push(new am4charts.PieSeries());
-    pieSeries2.dataFields.value = "value";
-    pieSeries2.dataFields.category = "name";
-
-    pieSeries2.slices.template.states.getKey(
-      "active"
-    ).properties.shiftRadius = 0;
-
-    //pieSeries2.labels.template.radius = am4core.percent(50);
-    //pieSeries2.labels.template.inside = true;
-    //pieSeries2.labels.template.fill = am4core.color("#ffffff");
-    pieSeries2.labels.template.disabled = true;
-    pieSeries2.ticks.template.disabled = true;
-    pieSeries2.alignLabels = false;
-    // eslint-disable-next-line
-    pieSeries2.events.on("positionchanged", updateLines);
-
-    const interfaceColors = new am4core.InterfaceColorSet();
-
-    const line1 = container.createChild(am4core.Line);
-    line1.strokeDasharray = "2,2";
-    line1.strokeOpacity = 0.5;
-    line1.stroke = interfaceColors.getFor("alternativeBackground");
-    line1.isMeasured = false;
-
-    const line2 = container.createChild(am4core.Line);
-    line2.strokeDasharray = "2,2";
-    line2.strokeOpacity = 0.5;
-    line2.stroke = interfaceColors.getFor("alternativeBackground");
-    line2.isMeasured = false;
-
-    let selectedSlice: any;
-
-    function updateLines() {
-      if (selectedSlice) {
-        let p11 = {
-          x: selectedSlice.radius * am4core.math.cos(selectedSlice.startAngle),
-          y: selectedSlice.radius * am4core.math.sin(selectedSlice.startAngle)
-        };
-        let p12 = {
-          x:
-            selectedSlice.radius *
-            am4core.math.cos(selectedSlice.startAngle + selectedSlice.arc),
-          y:
-            selectedSlice.radius *
-            am4core.math.sin(selectedSlice.startAngle + selectedSlice.arc)
-        };
-
-        p11 = am4core.utils.spritePointToSvg(p11, selectedSlice);
-        p12 = am4core.utils.spritePointToSvg(p12, selectedSlice);
-
-        let p21 = { x: 0, y: -pieSeries2.pixelRadius };
-        let p22 = { x: 0, y: pieSeries2.pixelRadius };
-
-        p21 = am4core.utils.spritePointToSvg(p21, pieSeries2);
-        p22 = am4core.utils.spritePointToSvg(p22, pieSeries2);
-
-        line1.x1 = p11.x;
-        line1.x2 = p21.x;
-        line1.y1 = p11.y;
-        line1.y2 = p21.y;
-
-        line2.x1 = p12.x;
-        line2.x2 = p22.x;
-        line2.y1 = p12.y;
-        line2.y2 = p22.y;
-      }
-    }
-
-    function selectSlice(dataItem: any) {
-      selectedSlice = dataItem.slice;
-
-      const fill = selectedSlice.fill;
-
-      const count = dataItem.dataContext.subData.length;
-      pieSeries2.colors.list = [];
-      for (let i = 0; i < count; i++) {
-        pieSeries2.colors.list.push(fill.brighten((i * 2) / count));
-      }
-
-      chart2.data = dataItem.dataContext.subData;
-      pieSeries2.appear();
-
-      const middleAngle = selectedSlice.middleAngle;
-
-      const firstAngle = pieSeries.slices.getIndex(0).startAngle;
-
-      const animation = pieSeries.animate(
-        [
-          { property: "startAngle", to: firstAngle - middleAngle },
-          { property: "endAngle", to: firstAngle - middleAngle + 360 }
-        ],
-        600,
-        am4core.ease.sinOut
-      );
-
-      animation.events.on("animationprogress", updateLines);
-
-      selectedSlice.events.on("transformed", updateLines);
-
-      //  var animation = chart2.animate({property:"dx", from:-container.pixelWidth / 2, to:0}, 2000, am4core.ease.elasticOut)
-      //  animation.events.on("animationprogress", updateLines)
-    }
-
-    chart.events.on("datavalidated", function() {
-      setTimeout(function() {
-        selectSlice(pieSeries.dataItems.getIndex(0));
-      }, 1000);
-    });
+    // This creates initial animation
+    pieSeries.hiddenState.properties.opacity = 1;
+    pieSeries.hiddenState.properties.endAngle = -90;
+    pieSeries.hiddenState.properties.startAngle = -90;
 
     // Add legend
     chart.legend = new am4charts.Legend();
