fccff4bfa7fd343f30b7104e03c3a2cf5b4c18a8
Joshua Coda <joshua.coda@gmail.com>
2020-03-30 10:55:42 -0700

    fix

--- src/views/Admin/Projects/Index.vue
+++ src/views/Admin/Projects/Index.vue
@@ -5,7 +5,7 @@
         <v-data-table
           :headers="headers"
           :items="desserts"
-          sort-by="calories"
+          sort-by="projectStartDate"
           class="elevation-1"
         >
           <template v-slot:top>
@@ -184,24 +184,24 @@ export default Vue.extend({
         sortable: false,
         value: "name"
       },
-      { text: "Calories", value: "calories" },
+      { text: "Project Start Date", value: "projectStartDate" },
       { text: "Fat (g)", value: "fat" },
       { text: "Carbs (g)", value: "carbs" },
       { text: "Protein (g)", value: "protein" },
       { text: "Actions", value: "actions", sortable: false }
     ],
-    desserts: [],
+    desserts: [{}],
     editedIndex: -1,
     editedItem: {
       name: "",
-      calories: 0,
+      projectStartDate: new Date().toISOString().substr(0, 10),
       fat: 0,
       carbs: 0,
       protein: 0
     },
     defaultItem: {
       name: "",
-      calories: 0,
+      projectStartDate: new Date().toISOString().substr(0, 10),
       fat: 0,
       carbs: 0,
       protein: 0
@@ -234,70 +234,70 @@ export default Vue.extend({
       this.desserts = [
         {
           name: "Frozen Yogurt",
-          calories: 159,
+          projectStartDate: new Date().toISOString().substr(0, 10),
           fat: 6.0,
           carbs: 24,
           protein: 4.0
         },
         {
           name: "Ice cream sandwich",
-          calories: 237,
+          projectStartDate: new Date().toISOString().substr(0, 10),
           fat: 9.0,
           carbs: 37,
           protein: 4.3
         },
         {
           name: "Eclair",
-          calories: 262,
+          projectStartDate: new Date().toISOString().substr(0, 10),
           fat: 16.0,
           carbs: 23,
           protein: 6.0
         },
         {
           name: "Cupcake",
-          calories: 305,
+          projectStartDate: new Date().toISOString().substr(0, 10),
           fat: 3.7,
           carbs: 67,
           protein: 4.3
         },
         {
           name: "Gingerbread",
-          calories: 356,
+          projectStartDate: new Date().toISOString().substr(0, 10),
           fat: 16.0,
           carbs: 49,
           protein: 3.9
         },
         {
           name: "Jelly bean",
-          calories: 375,
+          projectStartDate: new Date().toISOString().substr(0, 10),
           fat: 0.0,
           carbs: 94,
           protein: 0.0
         },
         {
           name: "Lollipop",
-          calories: 392,
+          projectStartDate: new Date().toISOString().substr(0, 10),
           fat: 0.2,
           carbs: 98,
           protein: 0
         },
         {
           name: "Honeycomb",
-          calories: 408,
+          projectStartDate: new Date().toISOString().substr(0, 10),
           fat: 3.2,
           carbs: 87,
           protein: 6.5
         },
         {
           name: "Donut",
-          calories: 452,
+          projectStartDate: new Date().toISOString().substr(0, 10),
           fat: 25.0,
           carbs: 51,
           protein: 4.9
         },
         {
           name: "KitKat",
-          calories: 518,
+          projectStartDate: new Date().toISOString().substr(0, 10),
           fat: 26.0,
           carbs: 65,
           protein: 7
