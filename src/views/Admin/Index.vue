8b806dbfb64b98f05774d6810739d8e896f28a09
Joshua Coda <joshua.coda@gmail.com>
2020-04-06 15:38:25 -0700

    fix

--- src/views/Admin/Index.vue
+++ src/views/Admin/Index.vue
@@ -1,36 +1,21 @@
 <template>
   <div>
-    <v-tabs v-model="tab" fixed-tabs show-arrows>
-      <v-tabs-slider></v-tabs-slider>
-
-      <v-tab href="#tab-1">
-        Projects
-      </v-tab>
-
-      <v-tab href="#tab-2">
-        Organizations
-      </v-tab>
-
-      <v-tab href="#tab-3">
-        Funders
-      </v-tab>
+    <v-tabs v-model="tabs" fixed-tabs show-arrows>
+      <v-tab href="#one">Projects</v-tab>
+      <v-tab href="#two">Organizations</v-tab>
+      <v-tab href="#three">Funders</v-tab>
+      <v-tabs-slider color="pink"></v-tabs-slider>
     </v-tabs>
 
-    <v-tabs-items v-model="tab">
-      <v-tab-item :value="'tab-1'">
-        <v-card flat>
-          <projects></projects>
-        </v-card>
+    <v-tabs-items v-model="tabs">
+      <v-tab-item v-for="content in ['one']" :key="content" :value="content">
+        <projects></projects>
       </v-tab-item>
-      <v-tab-item :value="'tab-2'">
-        <v-card flat>
-          <organizations></organizations>
-        </v-card>
+      <v-tab-item v-for="content in ['two']" :key="content" :value="content">
+        <organizations></organizations>
       </v-tab-item>
-      <v-tab-item :value="'tab-3'">
-        <v-card flat>
-          <funders></funders>
-        </v-card>
+      <v-tab-item v-for="content in ['three']" :key="content" :value="content">
+        <funders></funders>
       </v-tab-item>
     </v-tabs-items>
   </div>
@@ -50,8 +35,24 @@ export default Vue.extend({
   },
   data() {
     return {
-      tab: null
+      fab: false,
+      hidden: false,
+      tabs: ""
     };
+  },
+  computed: {
+    activeFab() {
+      switch (this.tabs) {
+        case "one":
+          return { color: "success", icon: "mdi-plus" };
+        case "two":
+          return { color: "red", icon: "mdi-plus" };
+        case "three":
+          return { color: "green", icon: "mdi-plus" };
+        default:
+          return {};
+      }
+    }
   }
 });
 </script>
